a0f3bf13041cd10823bc981ba2fceabd0566eb6f
SebastianGavilanes <juasegav@espol.edu.ec>
2021-11-21 22:02:34 -0500

    Se creo en el paquete modelo empresa.java

---
+++ PRIMERAENTREGA_PPOO/modelo/Empresa.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Empresa{
+	private String webPage; //Pagina web de la empresa
+
+	public Empresa(String webPage){
+		this.webPage=webPage;
+	}
+	public String getWebPage(){
+		return webPage;
+	}
+}
