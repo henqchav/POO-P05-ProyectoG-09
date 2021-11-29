4ae0e57ec586176e18747f352fbc5ddfa74f5394
SebastianGavilanes <juasegav@espol.edu.ec>
2021-11-29 00:18:27 -0500

    Se actualizo Duenio.java

--- PROYECTO_POO_V2/Duenio.java
+++ PROYECTO_POO_V2/Duenio.java
@@ -2,6 +2,10 @@ package modelo;
 public class Duenio extends Persona{
   private String cedula;
   private String apellido;
+
+  public String getCedula(){
+	  return cedula;
+  }
   //Constructor de la clase hija
   public Duenio(String n, String d, String t, String ciu,String e, String cod, String ced, String ape){
     super(n, d, t, ciu, e, cod);
