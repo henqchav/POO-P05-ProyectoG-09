291fc9faa8a65edd18e5fa239f86ab2caab3189c
LincolnEspol96 <87031534+LincolnEspol96@users.noreply.github.com>
2021-11-29 00:35:05 -0500

    Add files via upload

---
+++ PROYECTO_POO_V2/Premio.java
@@ -0,0 +1,46 @@
+package modelo;
+
+public class Premio {
+    private String nivel;//1ro, 2do o 3ro 
+    private float monto;
+    private String adicional;//son productos de comidas
+
+    public Premio(String nivel, float monto, String adicional) {
+        this.nivel = nivel;
+        this.monto = monto;
+        this.adicional = adicional;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+
+    public String getAdicional() {
+        return adicional;
+    }
+
+    public void setAdicional(String adicional) {
+        this.adicional = adicional;
+    }
+
+    @Override
+    public String toString() {
+        return nivel + " del Premio:  monto=" + monto + ", adicional=" + adicional;
+    }
+
+
+    
+    
+}
