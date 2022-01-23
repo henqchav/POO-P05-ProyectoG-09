662ad294a818390045707a5f61e27100c6d67880
henqchav <93171012+henqchav@users.noreply.github.com>
2022-01-23 12:47:41 -0500

    Update DescripcionMascotaController.java

--- AvanceP/src/main/java/com/mycompany/avancep/DescripcionMascotaController.java
+++ AvanceP/src/main/java/com/mycompany/avancep/DescripcionMascotaController.java
@@ -1,9 +1,15 @@
 package com.mycompany.avancep;
 
+import java.time.LocalDate;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import modelo.Animal;
+import modelo.Duenio;
+import modelo.Mascota;
 
 public class DescripcionMascotaController {
 
@@ -21,6 +27,33 @@ public class DescripcionMascotaController {
 
     @FXML
     private Label fx_raza;
+    @FXML
+    public void cargafoto(String photo) {
+        if(photo != null){
+            Image image = new Image(getClass().getResource("files/" + photo).toExternalForm(), 200, 150, false, false);
+            fx_foto.setImage(image);
+        }else{
+            Alert alerta = new Alert(Alert.AlertType.WARNING);
+            alerta.setTitle("Atención");
+            alerta.setHeaderText("NO SE ENCONTRO NINGULA FOTO DE LA MASCOTA");
+            alerta.setContentText("Por favor, selecciona una mascota de la tabla");
+            alerta.showAndWait();
+            System.out.println("SELECCIONE UNA MASCOTA");
+        }
+        
+    }
+    public void llenarCampos(Mascota m) {
+        fx_name.setText(m.getNombre());
+        fx_born.setText(m.getFechaNacimiento());
+        fx_owner.setText(String.valueOf(Duenio.getDueniobyID(m.getId())));
+        fx_raza.setText(m.getRaza());
+        fx_name.setDisable(true);
+        fx_born.setDisable(true);
+        fx_owner.setDisable(true);
+        fx_raza.setDisable(true);
+        cargafoto(m.getFoto());
+        
+    }
 
     @FXML
     void switchToAdministrarMascota(ActionEvent event) {  
