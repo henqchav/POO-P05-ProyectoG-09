7692ce8feb99af7a3f6f2e6ea0fdb762261f1c84
SebastianGavilanes <juasegav@espol.edu.ec>
2022-01-23 11:40:20 -0500

    Se actualizaron archvivos

---
+++ AvanceP/src/main/java/com/mycompany/avancep/modelo/Duenio.java
@@ -0,0 +1,58 @@
+package com.mycompany.avancep.modelo;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Duenio extends Persona{
+  private String cedula;
+  private String apellido;
+
+  public String getCedula(){
+    return cedula;
+  }
+  
+  public String getApellido(){
+    return apellido;
+  }
+  //Constructor de la clase hija
+  public Duenio(int id, String ape,String n, String d, String t, String ciu,String e){
+    super(n, d, t, ciu, e, id);
+    this.apellido = ape;
+  }
+  @Override
+  public String toString(){
+    return super.toString() + "\ncedula: " + cedula ; 
+  }
+  
+  
+   public static ArrayList<Duenio> cargarDuenios() {
+        ArrayList<Duenio> Duenios = new ArrayList<>();
+        //InputStream input = new FileInputStream("src/main/resources/com/mycompany/avancep/files/"+ "mascotas.csv");
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/resources/com/mycompany/avancep/files/"+ "duenosP5.csv")))) {
+            br.readLine();
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                String[] datos = line.split(",");
+                int id = Integer.valueOf(datos[0]);
+                Duenio d = new Duenio(id,datos[1],datos[2],datos[3],datos[4],datos[5],datos[6]);
+                //System.out.println("Duenio" + p);
+                Duenios.add(d);
+                line = br.readLine();
+            }
+            
+            
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return Duenios;
+
+    }
+  
+  
+  
+}
