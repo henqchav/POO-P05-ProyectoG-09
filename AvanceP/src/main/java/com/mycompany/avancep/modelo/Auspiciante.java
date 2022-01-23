db5fc4e00e7a1d978537c0136a670170a336602b
henqchav <93171012+henqchav@users.noreply.github.com>
2022-01-23 16:48:58 -0500

    Update Auspiciante.java

--- AvanceP/src/main/java/com/mycompany/avancep/modelo/Auspiciante.java
+++ AvanceP/src/main/java/com/mycompany/avancep/modelo/Auspiciante.java
@@ -1,4 +1,4 @@
-package com.mycompany.avancep.modelo;
+package com.mycompany.modelo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,46 +6,50 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import com.mycompany.modelo.Persona;
+import java.io.FileInputStream;
 
+public class Auspiciante extends Persona {
 
-public class Auspiciante extends Persona{
-  private String webpage;
-  //Constructor de la clase hija
-  
-  public Auspiciante(String n, String d, String t, String ciu,String e, String cod, String web){
-    super(n, d, t, ciu, e, cod);
-    this.webpage = web;
-    
-  }
-  @Override
-  public String toString(){
-    return super.toString() + "\npagina web: " + webpage; 
-  }
-  
-  
-  //Formato de Archivo
-  //cod, nombre, direccion, telefono, ciudad, email, webpage
-  
-  public static ArrayList<Auspiciante> cargarAuspiciantes(String ruta) {
+    private String webpage;
+    //Constructor de la clase hija
+    public String getWebpage(){
+        return webpage;
+    }
+
+    public Auspiciante(int id, String n,String d, String t, String ciu, String e, String web) {
+        super(n, d, t, ciu, e, id);
+        this.webpage = web;
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\npagina web: " + webpage;
+    }
+
+    //Formato de Archivo
+    //cod, nombre, direccion, telefono, ciudad, email, webpage
+    public static ArrayList<Auspiciante> cargarAuspiciantes() {
         ArrayList<Auspiciante> auspiciantes = new ArrayList<>();
-        InputStream input = Auspiciante.class.getClassLoader().getResourceAsStream(ruta);
-
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(input)))
-         {
+        //InputStream input = new FileInputStream("src/main/resources/com/mycompany/avancep/files/"+ "mascotas.csv");
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/resources/com/mycompany/avancep/files/"+ "auspiciantes.csv")))) {
+            br.readLine();
             String line = br.readLine();
             while (line != null) {
                 System.out.println(line);
-                String[] datos = line.split(",");
-                Auspiciante auspiciante = new Auspiciante(datos[1], datos[2], datos[3], datos[4], datos[5], datos[0],datos[6]);
-                auspiciantes.add(auspiciante);
+                String[] datos = line.split(";");
+                int id = Integer.valueOf(datos[0]);
+                Auspiciante a = new Auspiciante(id, datos[1],datos[2], datos[3], datos[4], datos[5], datos[6]);
+                //System.out.println("Mascota" + m);
+                auspiciantes.add(a);
                 line = br.readLine();
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } 
+            
+            
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
         return auspiciantes;
-    }
 
-  
-}
+    }}
